65a66551d53f58fe4484e38b1f0beadf52fd096d
ogurtsovam <ogurts.maria@gmail.com>
2024-11-19 19:56:23 +0200

    Add format feature

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,12 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+    console.log(`Using format: ${options.format}`);
+  });
 
 program.parse(process.argv);
-
