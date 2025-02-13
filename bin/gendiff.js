5fe0e2d6e0c6ee7801ccf29057f19189ff19015e
ogurtsovam <ogurts.maria@gmail.com>
2025-02-14 01:51:21 +0200

    Test-fix 4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import gendiff from '../src/gendiff.js';
+import * as genDiff from '../src/gendiff.js';
 
 program
   .name('gendiff')
@@ -9,7 +9,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const resultDiff = gendiff(filepath1, filepath2, program.opts().format);
+    const resultDiff = genDiff(filepath1, filepath2, program.opts().format);
     console.log(resultDiff);
   });
 
