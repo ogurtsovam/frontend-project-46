4b91ee7daef3f1dd90d997376820647ad2a87990
ogurtsovam <ogurts.maria@gmail.com>
2025-02-21 15:00:49 +0200

    Linter fix 6

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,14 @@ import { program } from 'commander';
 import genDiff from '../src/gendiff.js';
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    const resultDiff = genDiff(filepath1, filepath2, program.opts().format);
-    console.log(resultDiff);
-  });
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .action((filepath1, filepath2) => {
+        const resultDiff = genDiff(filepath1, filepath2, program.opts().format);
+        console.log(resultDiff);
+    });
 
 program.parse(process.argv);
